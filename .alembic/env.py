aa29dc643aa296c380d017aa23c1b7a51d14ad18
fka-kafka <kigenbrandon@gmail.com>
2024-07-26 14:51:26 +0300

    fixed unterminated string literal

--- .alembic/env.py
+++ .alembic/env.py
@@ -12,8 +12,7 @@ from app.config import settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', f"postgresql://{settings.db_username}:{
-                       settings.db_password}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}")
+config.set_main_option('sqlalchemy.url', f"postgresql://{settings.db_username}:{settings.db_password}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
